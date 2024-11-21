3cebb560aef626a3b7967a288cd3966327f5371b
Lucas Lima <lucas_lp1@outlook.com.br>
2024-11-20 21:44:25 -0300

    StreamWriter

---
+++ 030. Stream Writer/Course/Course/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             É uma stream capaz de escrever caracteres a partir de uma stream binária (Ex. FileStream)
+             */
+
+            string sourcePath = @"C:\Users\lucas.porto\OneDrive - HARALD INDUSTRIA E COMERCIO DE ALIMENTOS SA\Documentos\Estágio\Pessoal\CursoCSharp\CursoCSharp--Tempo-Limitado-\030. Stream Writer\file1.txt";
+            string targetPath = @"C:\Users\lucas.porto\OneDrive - HARALD INDUSTRIA E COMERCIO DE ALIMENTOS SA\Documentos\Estágio\Pessoal\CursoCSharp\CursoCSharp--Tempo-Limitado-\030. Stream Writer\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error Ocurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
